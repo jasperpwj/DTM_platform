a746412e28fa7837675cc6bbec5c2f07d50d5fdf
TianCccc <34478443+TianCccc@users.noreply.github.com>
2021-03-30 00:15:23 -0400

    mytime

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -20,13 +20,11 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-
 export default function Home(props) {
     const classes = useStyles();
 
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-
+    // const [currentDate, setCurrentDate] = useState;
     useEffect(()=> {
         let currentUser = authService.getCurrentUser();
         if(currentUser) {
@@ -41,6 +39,23 @@ export default function Home(props) {
     const navInfo = {
         button: "home"
     };
+
+
+    var date = new Date();
+    var sep = "-";
+    var year = date.getFullYear(); //获取完整的年份(4位)
+    var month = date.getMonth() + 1; //获取当前月份(0-11,0代表1月)
+    var day = date.getDate(); //获取当前日
+    if (month <= 9) {
+        month = "0" + month;
+    }
+    if (day <= 9) {
+        day = "0" + day;
+    }
+    var currentdate = year + sep + month + sep + day;
+        
+    
+
     return (
         <div className={classes.root}>
             <main className={classes.content}>
@@ -50,6 +65,9 @@ export default function Home(props) {
                         This is home page
                     </Typography>
                 </div>
+                    mytime: 
+                    <b>{currentdate}</b>
+                
             </main>
 
         </div>
