77646338d408cc8f2b5ff0d56d2620e2f1997c98
jasperP <53663744+jasperpwj@users.noreply.github.com>
2021-04-26 00:37:11 -0400

    refactor dashboard design and implementation

--- controllers/user.helper.js
+++ controllers/user.helper.js
@@ -1,5 +1,6 @@
 const mongoCollection = require("../config/mongoCollections");
 const users = mongoCollection.users;
+const { ObjectId } = require("mongodb");
 
 async function validateEmail(email) {
     const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -13,9 +14,16 @@ async function getUserByEmail(email) {
     if (!user) throw 'No user Found';
     return user;
 }
-
+async function getUserNameByUserId(userId) {
+    // const userMongoId = ObjectId.createFromHexString(userId);
+    const usersCollection = await users();
+    let user = await usersCollection.findOne({_id: userId});
+    if(!user) throw `Cannot find user by id: ${userId}`;
+    return user.firstName + " " + user.lastName;
+}
 
 module.exports = {
     validateEmail,
     getUserByEmail,
+    getUserNameByUserId,
 };
